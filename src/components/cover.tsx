bb01bdfc0e9da55be883a6e4b4a1d4ae239d5f6a
Jinyoung <roghabo@gmail.com>
2021-10-04 20:02:08 +0900

    scroll 적용

--- src/components/cover.tsx
+++ src/components/cover.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import mainImg from "../images/mainImg.png";
 
 export const Cover = () => {
+  const [scrollTop, setScrollTop] = useState(0);
+  useEffect(() => {
+    const ssss = (e: any) => {
+      if (e.target.scrollTop <= 36) {
+        setScrollTop(e.target.scrollTop);
+      }
+    };
+    window.addEventListener("scroll", ssss, true);
+    return () => window.removeEventListener("scroll", ssss, true);
+  }, []);
+
   return (
     <section className="cover">
       <img src={mainImg} alt="" />
-      <div className="cover__container">
+      <div
+        className="cover__container"
+        style={{ transform: `translateY(-${scrollTop}px)` }}
+      >
         <div className="cover__title">
           <span>2019.10.12</span>
         </div>

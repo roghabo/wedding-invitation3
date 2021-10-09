a7e199a806e928897d0c010dad3a8ccf3e7050a6
Jinyoung <roghabo@gmail.com>
2021-10-09 18:14:24 +0900

    Introduction 모듈화

--- src/components/introduction.tsx
+++ src/components/introduction.tsx
@@ -1,7 +1,19 @@
 import React, { useEffect, useState } from "react";
 import Typewriter from "typewriter-effect";
 
-export const Introduction = () => {
+interface IIntroductionProps {
+  name: string;
+  text: any;
+  groomFamily: string;
+  brideFamily: string;
+}
+
+export const Introduction: React.FC<IIntroductionProps> = ({
+  name,
+  text,
+  groomFamily,
+  brideFamily,
+}) => {
   const [effectStart, seteffectStart] = useState(false);
   useEffect(() => {
     const setEffect = (e: any) => {
@@ -18,9 +30,7 @@ export const Introduction = () => {
         {effectStart && (
           <Typewriter
             onInit={(typewriter) => {
-              typewriter
-                .typeString("안녕하세요. <br /> 정영석, 최수지입니다.")
-                .start();
+              typewriter.typeString(name).start();
             }}
             options={{
               autoStart: false,
@@ -28,19 +38,10 @@ export const Introduction = () => {
           />
         )}
       </div>
-      <div className="introduction__content">
-        <p>
-          서로가 마주 보며 다져온 사랑을 <br />
-          이제 함께 한 곳을 바라보며 걸어갈 수 있는 <br />
-          큰 사랑으로 키우고자 합니다. <br />
-          저희 두 사람이 <br />
-          사랑의 이름으로 지켜나갈 수 있게 <br />
-          앞날을 축복해 주시면 감사하겠습니다.
-        </p>
-      </div>
+      <div className="introduction__content">{text}</div>
       <div className="introduction__people">
-        정OO·송OO 의 장남 정영석 <br />
-        최OO·강OO 의 차녀 최수지
+        {groomFamily} <br />
+        {brideFamily}
       </div>
     </section>
   );
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -40,7 +40,21 @@ export const Main = () => {
             mainImg={mainImg}
           />
           <Image />
-          <Introduction />
+          <Introduction
+            name="안녕하세요. <br /> 정영석, 최수지입니다."
+            text={
+              <span>
+                서로가 마주 보며 다져온 사랑을 <br />
+                이제 함께 한 곳을 바라보며 걸어갈 수 있는 <br />
+                큰 사랑으로 키우고자 합니다. <br />
+                저희 두 사람이 <br />
+                사랑의 이름으로 지켜나갈 수 있게 <br />
+                앞날을 축복해 주시면 감사하겠습니다.
+              </span>
+            }
+            groomFamily="정OO·송OO 의 장남 정영석"
+            brideFamily="최OO·강OO 의 차녀 최수지"
+          />
           <Gallery />
           <Location />
           <Guest

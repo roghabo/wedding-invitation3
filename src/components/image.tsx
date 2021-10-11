8cbb03f0c1d12cab4a2f1412ff863541cf06ab69
Jinyoung <roghabo@gmail.com>
2021-10-11 17:34:25 +0900

    image scale

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,13 +1,41 @@
-import React from "react";
-import man from "../images/man.png";
-import woman from "../images/woman.png";
+import React, { useEffect, useState } from "react";
+import groom from "../images/groom.png";
+import bride from "../images/bride.png";
 
 export const Image = () => {
+  const [groomScale, setGroomScale] = useState(1);
+  const [brideScale, setBrideScale] = useState(1);
+  useEffect(() => {
+    const scrollEvent = (e: any) => {
+      console.log(e.target.scrollTop);
+      if (e.target.scrollTop > 10 && e.target.scrollTop <= 326) {
+        setGroomScale(1 + e.target.scrollTop / 3160);
+      }
+      if (e.target.scrollTop > 50 && e.target.scrollTop <= 366) {
+        setBrideScale(1 + e.target.scrollTop / 3160);
+      }
+    };
+    window.addEventListener("scroll", scrollEvent, true);
+    return () => window.removeEventListener("scroll", scrollEvent, true);
+  }, []);
   return (
     <section className="image">
       <div className="image__container">
-        <img src={man} alt="" />
-        <img className="image__woman" src={woman} alt="" />
+        <div className="image__container__image">
+          <img
+            src={groom}
+            alt=""
+            style={{ transform: `scale(${groomScale}, ${groomScale})` }}
+          />
+        </div>
+        <div className="image__container__image">
+          <img
+            className="image__bride"
+            src={bride}
+            alt=""
+            style={{ transform: `scale(${brideScale}, ${brideScale})` }}
+          />
+        </div>
       </div>
     </section>
   );
